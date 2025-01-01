e4f803c4b7464ba80163a4d38ef6e305ba9bb56b
Pavani513 <pavanireddypulicharla@gmail.com>
2025-01-02 00:18:18 +0530

    Update lollipop.js

--- Lollipops/lollipop.js
+++ Lollipops/lollipop.js
@@ -167,7 +167,6 @@ lollipopCards.forEach((item, index) => {
     e.stopImmediatePropagation();
     addToCart(index);
     Swal.fire({
-      title: "Added to Cart!",
       text: "The item has been successfully added to your cart.",
       icon: "success",
       timer: 1500,
